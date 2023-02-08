d176ab18fc4c180afebf5a14ca249331a4b90870
sandro <sandrosilva9029@gmail.com>
2023-02-08 13:42:06 -0300

    checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -50,7 +50,7 @@ public class UI {
 			throw new InputMismatchException("Error reading chess position. Valid Values are from a1 to h8.");
 		}
 	}
-	
+
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
@@ -58,10 +58,16 @@ public class UI {
 		System.out.println();
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("Check!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("Check!");
+			}
+		} else {
+			System.out.println("CHECKMATE!");
+			System.out.println(chessMatch.getCurrentPlayer() + " WON!");
 		}
+
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -74,7 +80,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -101,17 +107,19 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 	private static void printCapturedPieces(List<ChessPiece> captured) {
-		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+				.collect(Collectors.toList());
+		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+				.collect(Collectors.toList());
 
 		System.out.println("Captured pieces: ");
 		System.out.print(ANSI_WHITE);
 		System.out.print("White: ");
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
-		
+
 		System.out.print(ANSI_GREEN);
 		System.out.print("Black: ");
 		System.out.println(Arrays.toString(black.toArray()));
